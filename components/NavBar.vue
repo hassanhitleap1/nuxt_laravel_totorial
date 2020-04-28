92b3e7419e88aceba0e4e75788e4d57158fd5f0a
hassan kiwan <hassanhitleap1@gmail.com>
2020-04-28 12:25:00 +0300

    2

---
+++ components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nuxt-link class="navbar-brand" to="/"> app nuxt with laravel</nuxt-link>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <nuxt-link class="nav-link" to="/"> home <span class="sr-only">(current)</span></nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link class="nav-link" to="/link"> Link</nuxt-link>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+</template>
